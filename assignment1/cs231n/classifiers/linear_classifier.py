695da19500e954944249b5e086f52bfe14ac64d5
Ken Chan <kenchan0824@gmail.com>
2019-03-03 16:05:39 +0800

    Complete backprop for softmax.

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -53,7 +53,9 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      idx = np.random.choice(num_train, batch_size, replace=True)
+      X_batch = X[idx]  # (batch_size x dim)
+      y_batch = y[idx]
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -67,7 +69,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W += -learning_rate * grad # D,C
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -96,7 +98,8 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    score = np.dot(X, self.W) # N,D x D,C = N,C
+    y_pred = np.argmax(score, axis=1) # N,1
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -3,61 +3,108 @@ from random import shuffle
 from past.builtins import xrange
 
 def softmax_loss_naive(W, X, y, reg):
-  """
-  Softmax loss function, naive implementation (with loops)
+    """
+    Softmax loss function, naive implementation (with loops)
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    N, D = X.shape
 
-  return loss, dW
+    for i in xrange(N):
+
+        # forward pass
+        score = np.dot(X[i, :], W)
+        exp = np.exp(score)
+        expsum = np.sum(exp)
+        prob = exp / expsum
+        loss += -np.log(prob[y[i]])
+
+        # back pass
+        dprob = np.zeros_like(prob)
+        dprob[y[i]] = -1.0 / prob[y[i]]
+        dexpsum = -1.0 / expsum**2 * np.sum(exp * dprob)
+        dexp = 1.0 / expsum * dprob + np.ones_like(exp) * dexpsum
+        dscore = exp * dexp
+        dW += np.outer(X[i, :], dscore)
+
+    loss /= N
+    dW /= N
+
+    # regularization
+    loss += 0.5 * reg * np.sum(W * W)
+    dW += reg * W
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
 
 
 def softmax_loss_vectorized(W, X, y, reg):
-  """
-  Softmax loss function, vectorized version.
+    """
+    Softmax loss function, vectorized version.
 
-  Inputs and outputs are the same as softmax_loss_naive.
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Inputs and outputs are the same as softmax_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    N, D = X.shape
+    _, C = W.shape
 
-  return loss, dW
+    # forward pass
+    onehot = np.eye(C)[y] # N,C
+    score = np.dot(X, W) # N,D x D,C = N,C
+    score -= np.max(score, axis=1).reshape(N, 1) # stabilize f
+    exp = np.exp(score) # N,C
+    expsum = np.sum(exp, axis=1).reshape(N, 1) # N,1
+    prob = exp / expsum # N,C
+    loss = -np.log(prob[onehot==1]) # N,1
+
+    # back pass
+    dproby = (-1.0 / prob[onehot==1]).reshape(N, 1) # N,1
+    dexpsum = -1.0 / expsum**2 * exp[onehot==1].reshape(N, 1) * dproby # N,1
+    dexp = onehot / expsum * dproby + np.ones_like(exp) * dexpsum # N,C
+    dscore = exp * dexp # N,C
+    dW = np.dot(X.T, dscore) # D,C
+
+    loss = np.sum(loss) / N
+    dW /= N
+
+    loss += 0.5 * reg * np.sum(W * W)
+    dW += reg * W
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
 
